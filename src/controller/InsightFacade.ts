546e870daa1f69458b4579ad20adac0124316e18
Mobileheo <mobileheo@gmail.com>
2017-01-27 01:40:00 -0800

    @CPSC310bot

--- src/controller/InsightFacade.ts
+++ src/controller/InsightFacade.ts
@@ -24,6 +24,61 @@ let query:QueryRequest = {"WHERE":
     }
 };
 
+
+function filter(query:any) {
+    let keys = Object.keys(query);
+    for (let key of keys) {
+        if (key == "WHERE") {
+            let whereVal = query[key];
+            let whereKeys = Object.keys(whereVal);
+            for (let wherekey of whereKeys) {
+                let gtVal = whereVal[wherekey];
+                let gtKeys = Object.keys(gtVal);
+                if (wherekey == 'OR') {
+                    for (let gtKey of gtKeys) {
+                        if (gtKey == 'OR') {
+
+                        }
+
+                    }
+
+                }
+            }
+        }
+    }
+}
+
+function convert_to_code(key:string ):string {
+    if (key == 'courses_avg') {
+        return 'Avg';
+    }
+    else if (key == 'courses_dept') {
+        return 'Subject';
+    }
+    else if (key == 'courses_id') {
+        return 'Course';
+    }
+    else if (key == 'courses_instructor') {
+        return 'Professor';
+    }
+    else if (key == 'courses_title') {
+        return 'title';
+    }
+    else if (key == 'courses_pass') {
+        return 'Pass';
+    }
+    else if (key == 'courses_fail') {
+        return 'Fail';
+    }
+    else if (key == 'courses_audit') {
+        return 'Audit';
+    }
+    else if (key == 'courses_uuid') {
+        return 'id';
+    }
+}
+
+
 function string_to_op(s_op:string, k:number):boolean {
     let x = Number(s_op.substring(2));
     if (s_op.charAt(1) == '>') {
@@ -55,9 +110,7 @@ function classify(s_op:string):string {
 
 }
 
-
-
-function q(query:any) {
+function mathComparison(query:any) {
     let keys = Object.keys(query);
     for (let key of keys) {
         if (key == "WHERE") {
@@ -127,6 +180,80 @@ function q(query:any) {
     }
 }
 
+function sComparison(query:any) {
+    let keys = Object.keys(query);
+    for (let key of keys) {
+        if (key == "WHERE") {
+            let whereVal = query[key];
+            let whereKeys = Object.keys(whereVal);
+            for (let wherekey of whereKeys) {
+                let gtVal = whereVal[wherekey];
+                let gtKeys = Object.keys(gtVal);
+                if (wherekey == 'IS') {
+                    for (let gtKey of gtKeys) {
+                        if (gtKey == "courses_dept") {
+                            let courses_dept: string = gtVal[gtKey];
+                            return courses_dept;
+                        }
+                        else if (gtKey == "courses_id") {
+                            let courses_id: string = gtVal[gtKey];
+                            return courses_id;
+                        }
+                        else if (gtKey == "courses_instructor") {
+                            let courses_instructor: string = gtVal[gtKey];
+                            return courses_instructor;
+                        }
+                        else if (gtKey == "courses_title") {
+                            let courses_title: string = gtVal[gtKey];
+                            return courses_title;
+                        }
+                        else if (gtKey == "courses_uuid") {
+                            let courses_uuid: string = gtVal[gtKey];
+                            return courses_uuid;
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
+function return_LTGTEQ(query:any) {
+    let keys = Object.keys(query);
+    for (let key of keys) {
+        if (key == 'WHERE') {
+            let where_Contents = query[key];
+            for (let key of Object.keys(where_Contents)) {
+                if (key == 'LT') {
+                    return 'LT';
+                }
+                else if (key == 'GT') {
+                    return 'GT';
+                }
+                else if (key == 'EQ') {
+                    return 'EQ';
+                }
+            }
+        }
+    }
+}
+
+function return_LTGTEQ_Key(query:any):string {
+    let key = return_LTGTEQ(query);
+    let where_Contents = query["WHERE"];
+    let LTGTEQ_Key = Object.keys(where_Contents[key])[0];
+    return LTGTEQ_Key; //avg
+
+
+}
+
+function return_LTGTEQ_Key_Value(query:any):number {
+    let key = return_LTGTEQ(query);
+    let where_Contents = query["WHERE"];
+    let LTGTEQ_Key_Value = where_Contents[key][return_LTGTEQ_Key(query)];
+    return LTGTEQ_Key_Value; //score
+}
+
 function return_columns(query:any) {
     let keys = Object.keys(query);
     for (let key of keys) {
@@ -147,8 +274,52 @@ function return_columns(query:any) {
     }
 }
 
+function return_Order(query:any) {
+    let keys = Object.keys(query);
+    for (let key of keys) {
+        if (key == "OPTIONS") {
+            let optionsVal = query[key]; // { COLUMNS: [ 'courses_dept', 'courses_avg' ],ORDER: 'courses_avg', FORM: 'TABLE' }
+            // console.log(optionsVal);
+            let optionsKeys = Object.keys(optionsVal);
+            console.log(optionsKeys);
+            for (let optionsKey of optionsKeys){
+                if (optionsKey == "ORDER"){
+                    let optionVal = optionsVal[optionsKey]; //[ 'courses_dept', 'courses_avg' ]
+                    return optionVal;
+                }
 
-export default class InsightFacade implements IInsightFacade {
+            }
+
+        }
+    }
+}
+
+function return_Form(query:any) {
+    let keys = Object.keys(query);
+    for (let key of keys) {
+        if (key == "OPTIONS") {
+            let optionsVal = query[key]; // { COLUMNS: [ 'courses_dept', 'courses_avg' ],ORDER: 'courses_avg', FORM: 'TABLE' }
+            // console.log(optionsVal);
+            let optionsKeys = Object.keys(optionsVal);
+            console.log(optionsKeys);
+            for (let optionsKey of optionsKeys){
+                if (optionsKey == "FORM"){
+                    let optionVal = optionsVal[optionsKey]; //[ 'courses_dept', 'courses_avg' ]
+                    return optionVal;
+                }
+
+            }
+
+        }
+    }
+}
+
+
+
+
+
+
+        export default class InsightFacade implements IInsightFacade {
 
     constructor() {
         Log.trace('InsightFacadeImpl::init()')
@@ -198,53 +369,70 @@ export default class InsightFacade implements IInsightFacade {
     }
     performQuery(query: QueryRequest): Promise <InsightResponse> {
         return new Promise(function (fulfill, reject) {
-            var foo = q(query);
+            let foo = mathComparison(query);
             console.log(foo);
-            var columns = return_columns(query);
+            let columns = return_columns(query);
             console.log(columns);
-            let output: any = {'result': []};
-            // let promiseList: Promise<any>[] =[];
+            let order = return_Order(query);
+            console.log("order is " + order);
+            let form = return_Form(query);
+            console.log("Form is " + form);
+            let output: any = {'render': '','result': []};
+            let mcomparion = return_LTGTEQ(query);
+            let mcomparison_Key = return_LTGTEQ_Key(query);
+            let mcomparison_Key_Value = return_LTGTEQ_Key_Value(query);
+            let convtered_code = convert_to_code(mcomparison_Key);
+            if (form != undefined) {
+                output['render'] = form;
+            }
             var data = fs.readFileSync('testfile.json', 'utf-8');
             var parse_data:any = JSON.parse(data);
+            let total:any= {};
             for (let item of parse_data) {
                 var keys = Object.keys(item);
                 var course_info = item[keys[0]];
-                for (let each of course_info){
+                //console.log(course_info);
+                for (let each of course_info) {
                     var keys = Object.keys(each);
-                    for (let key of keys) {
-                        if (classify(foo) == 'Avg' ) {
-                            if (key == 'Avg' && string_to_op(foo, each['Avg']) && columns == undefined) {
-                                output['result'].push({courses_avg: each[key]});
+                    for (let item of keys) {
+                        //console.log(item);
+                        if(item == convtered_code) {
+                            // console.log(item);
+                            if (mcomparion == 'LT') {
+                                if (mcomparison_Key_Value > each[convtered_code]) {
+                                    for (let column of columns) {
+                                        Object.assign(total, {[column]: each[convert_to_code(column)]});
+                                        output['result'].push(total);
+                                    }
+
+                                }
+                                // output['result'].push(total);
                             }
-                            else if (key == 'Avg' && string_to_op(foo, each['Avg']) && columns != undefined) {
-                                for (let column of columns) {
-                                    if (column == "courses_dept") {
-                                        output['result'].push({courses_dept: each['Subject'], courses_avg: each[key]});
+
+                            else if (mcomparion == 'GT') {
+                                if (mcomparison_Key_Value < each[convtered_code]) {
+                                    for (let column of columns) {
+                                        Object.assign(total, {[column]: each[convert_to_code(column)]});
+                                        // output['result'].push(total);
+                                    }
+
+                                }
+                                // output['result'].push(total);
+                            }
+                            else if (mcomparion == 'QT') {
+                                if (mcomparison_Key_Value == each[convtered_code]) {
+                                    for (let column of columns) {
+                                        Object.assign(total, {[column]: each[convert_to_code(column)]});
+                                        output['result'].push(total);
                                     }
                                 }
                             }
-                        }
-                        else if (classify(foo) == 'Pass') {
-                            if (key == 'Pass' && string_to_op(foo, each[key])) {
-                                output['result'].push({courses_pass: each[key]});
-                            }
-                        }
-                        else if (classify(foo) == 'Fail') {
-                            if (key == 'Fail' && string_to_op(foo, each['Fail'])) {
-                                output['result'].push({courses_fail: each[key]});
-                            }
-                        }
-                        else if (classify(foo) == 'Audit') {
-                            if (key == 'Audit' && string_to_op(foo, each['Audit'])) {
-                                output['result'].push({courses_audit: each[key]});
-
-                            }
+                            output['result'].push(total);
                         }
                     }
-
                 }
             }
-
+            // output['result'].push(total);
             console.log(output);
             // reject({code: 400, body: {"error": "my text"}});
             fulfill({code: 201, body: {}})
