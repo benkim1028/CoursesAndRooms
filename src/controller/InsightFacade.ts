bd5fb5a82069e2cf1f44fe0fb790263e5b5ab33a
Ben Kim <Ben Kim>
2017-02-08 00:08:32 -0800

    TEST

--- src/controller/InsightFacade.ts
+++ src/controller/InsightFacade.ts
@@ -17,178 +17,217 @@ var Doeverything: DoEveryThing = null;
 var counter: boolean = true;
 function whatKindofFilter(input: any, value: any, preList: any = []) {
     // this takes filter and its
-    if (input == "OR") {//logic comparison
-        if (value.length == 0){
-            Doeverything.fail = true;
-            return [];
+    try {
+        if (input == "OR") {//logic comparison
+            if (value.length == 0) {
+                Doeverything.fail = true;
+                return [];
+            }
+            return createORList(value, preList);
+        } else if (input == "AND") {
+            if (value.length == 0) {
+                Doeverything.fail = true;
+                return [];
+            }
+            return createANDList(value, preList);
         }
-        return createORList(value, preList);
-    } else if (input == "AND") {
-        if (value.length == 0){
-            Doeverything.fail = true;
-            return [];
+        var subKey: any = Object.keys(value);
+        var subValue: any = value[subKey[0]];
+        if (input == 'GT') {    // MComparison
+            return createGTList(subKey[0], subValue, preList);
         }
-        return createANDList(value, preList);
-    }
-    var subKey: any = Object.keys(value);
-    var subValue: any = value[subKey[0]];
-    if (input == 'GT') {    // MComparison
-        return createGTList(subKey[0], subValue, preList);
-    }
-    else if (input == 'LT') {
-        return createLTList(subKey[0], subValue, preList);
-    }
-    else if (input == 'EQ') {     //SComparison
-        return createEQList(subKey[0], subValue, preList);
-    }
-    else if (input == "IS") {
-        return createISList(subKey[0], subValue, preList);
-    } else if (input == "NOT") {
-        return createNOTList(value, preList);
+        else if (input == 'LT') {
+            return createLTList(subKey[0], subValue, preList);
+        }
+        else if (input == 'EQ') {     //SComparison
+            return createEQList(subKey[0], subValue, preList);
+        }
+        else if (input == "IS") {
+            return createISList(subKey[0], subValue, preList);
+        } else if (input == "NOT") {
+            return createNOTList(value, preList);
+        }
+    }catch(e) {
+        console.log(e);
     }
 }
 
 function createNOTList(value: any, dataList: any[]): any[] {
-    var key = Object.keys(value)[0];
-    var keyValue = value[key];
-    var response = whatKindofFilter(key, keyValue, dataList);
-    var sortedList: any[] = [];
-    for (var k = 0; k < response.length; k++) {
-        for(var j= 0; j < dataList.length; j++) {
-            if (response[k]["id"] != dataList[j]["id"])
-                sortedList.push(response[k]);
-        }
-    }
-    return sortedList;
-}
-function createORList(list: any[], preList: any[]): any[]{
-    var sortedList: any[] = [];
-    for(let i = 0; i < list.length; i++){
-        var keysOfObject = Object.keys(list[i]);
-        let response = whatKindofFilter(keysOfObject[0], list[i][keysOfObject[0]], preList);
-        if(sortedList.length == 0){
-            sortedList = response;
-        } else {
-            for (var j = 0; j < response.length; j++) {
-                var counter = 0;
-                for (var k = 0; k < sortedList.length; k++) {
-                    if (sortedList[k]["id"] == response[j]["id"]) {
-                        counter = 1;
-                        break;
-                    }
-                }
-                if (counter == 0)
-                    sortedList.push(response[j])
+    try {
+        var key = Object.keys(value)[0];
+        var keyValue = value[key];
+        var response = whatKindofFilter(key, keyValue, dataList);
+        var sortedList: any[] = [];
+        for (var k = 0; k < response.length; k++) {
+            for (var j = 0; j < dataList.length; j++) {
+                if (response[k]["id"] != dataList[j]["id"])
+                    sortedList.push(response[k]);
             }
         }
+        return sortedList;
     }
-    return sortedList;
+    catch(e) {
+        console.log(e);
+    }
+}
+function createORList(list: any[], preList: any[]): any[]{
+    try {
+        var sortedList: any[] = [];
+        for (let i = 0; i < list.length; i++) {
+            var keysOfObject = Object.keys(list[i]);
+            let response = whatKindofFilter(keysOfObject[0], list[i][keysOfObject[0]], preList);
+            if (sortedList.length == 0) {
+                sortedList = response;
+            } else {
+                for (var j = 0; j < response.length; j++) {
+                    var counter = 0;
+                    for (var k = 0; k < sortedList.length; k++) {
+                        if (sortedList[k]["id"] == response[j]["id"]) {
+                            counter = 1;
+                            break;
+                        }
+                    }
+                    if (counter == 0)
+                        sortedList.push(response[j])
+                }
+            }
+        }
+        return sortedList;
+    }
+    catch(e) {
+        console.log(e);
+    }
 }
 
 function createANDList(list: any[], preList: any[]): any[]{
-    var Initialized = false;
-    var resultlist: any[] = [];
-    for(let i = 0; i < list.length; i++){
-        var sortedList: any[] = [];
-        var keysOfObject = Object.keys(list[i]);
-        let response = whatKindofFilter(keysOfObject[0], list[i][keysOfObject[0]], preList);
-        if (!Initialized) {
-            resultlist = response;
-            Initialized = true;
-        } else {
-            for (var j = 0; j < response.length; j++) {
-                for (var k = 0; k < resultlist.length; k++) {
-                    if (resultlist[k]["id"] == response[j]["id"]) {
-                        sortedList.push(response[j]);
+    try {
+        var Initialized = false;
+        var resultlist: any[] = [];
+        for (let i = 0; i < list.length; i++) {
+            var sortedList: any[] = [];
+            var keysOfObject = Object.keys(list[i]);
+            let response = whatKindofFilter(keysOfObject[0], list[i][keysOfObject[0]], preList);
+            if (!Initialized) {
+                resultlist = response;
+                Initialized = true;
+            } else {
+                for (var j = 0; j < response.length; j++) {
+                    for (var k = 0; k < resultlist.length; k++) {
+                        if (resultlist[k]["id"] == response[j]["id"]) {
+                            sortedList.push(response[j]);
+                        }
                     }
                 }
+                resultlist = sortedList;
             }
-            resultlist = sortedList;
         }
+        return resultlist;
     }
-    return resultlist;
+    catch(e) {
+        console.log(e);
+    }
 }
 function createGTList(key: string, value: number, dataList: any[]): any[] {
-    var sortedList: any[] = [];
-    var realKey = findKey(key);
-    for(let i = 0; i < dataList.length; i++){
+    try {
+        var sortedList: any[] = [];
+        var realKey = findKey(key);
+        for (let i = 0; i < dataList.length; i++) {
 
-        if (typeof (dataList[i][realKey]) === 'number' && dataList[i][realKey] > value) {
-            sortedList.push(dataList[i]);
+            if (typeof (dataList[i][realKey]) === 'number' && dataList[i][realKey] > value) {
+                sortedList.push(dataList[i]);
+            }
+            else if (typeof (dataList[i][realKey]) !== 'number') {
+                Doeverything.fail = true;
+            }
         }
-        else if (typeof (dataList[i][realKey]) !== 'number' ) {
-            Doeverything.fail = true;
-        }
+        return sortedList;
     }
-    return sortedList;
+    catch(e) {
+        console.log(e);
+    }
 
 }
 function createLTList(key: string, value: number, dataList: any[]): any[] {
-    var sortedList: any[] = [];
-    var realKey = findKey(key);
-    for(let i = 0; i < dataList.length; i++){
-        if (typeof (dataList[i][realKey]) === 'number' && dataList[i][realKey] < value) {
-            sortedList.push(dataList[i]);
+    try {
+        var sortedList: any[] = [];
+        var realKey = findKey(key);
+        for (let i = 0; i < dataList.length; i++) {
+            if (typeof (dataList[i][realKey]) === 'number' && dataList[i][realKey] < value) {
+                sortedList.push(dataList[i]);
+            }
+            else if (typeof (dataList[i][realKey]) !== 'number') {
+                Doeverything.fail = true;
+            }
         }
-        else if (typeof (dataList[i][realKey]) !== 'number' ) {
-            Doeverything.fail = true;
-        }
+        return sortedList;
     }
-    return sortedList;
+    catch(e) {
+        console.log(e);
+    }
 }
 
 function createEQList(key: string, value: number, dataList: any[]): any[] {
-    var sortedList: any[] = [];
-    var realKey = findKey(key);
-    for(let i = 0; i < dataList.length; i++){
-        if (typeof (dataList[i][realKey]) === 'number' && dataList[i][realKey] == value) {
-            sortedList.push(dataList[i]);
+    try {
+        var sortedList: any[] = [];
+        var realKey = findKey(key);
+        for (let i = 0; i < dataList.length; i++) {
+            if (typeof (dataList[i][realKey]) === 'number' && dataList[i][realKey] == value) {
+                sortedList.push(dataList[i]);
+            }
+            else if (typeof (dataList[i][realKey]) !== 'number') {
+                Doeverything.fail = true;
+            }
         }
-        else if (typeof (dataList[i][realKey]) !== 'number' ) {
-            Doeverything.fail = true;
-        }
+        return sortedList;
     }
-    return sortedList;
+    catch(e) {
+        console.log(e);
+    }
 }
 
 function createISList(key: string, value: string, dataList: any[]): any[] {
-    var sortedList: any[] = [];
-    var realKey = findKey(key);
-    var firstcase = new RegExp("^\\*([a-z]+|(\\;|\\-|\\,))([a-z]|(\\;|\\-|\\s|\\,))*$");
-    var secondcase = new RegExp("^([a-z]|(\\;|\\-|\\s|\\,))*([a-z]+|(\\;|\\-|\\,))\\*$");
-    var thirdcase = new RegExp("^\\*([a-z]|(\\;|\\-|\\,))+([a-z]|(\\;|\\-|\\,|\\s))*\\*$");
-    //var regex = /[a-z]+(\\;|\\-|\\s)?[a-z]*(\\,[a-z]+(\\;|\\-|\\s)?[a-z]*)*/
-    for(let i = 0; i < dataList.length; i++){
-        if (typeof (dataList[i][realKey]) === 'string') {
-            if (firstcase.test(value)){
-                var res = firstcase.exec(value);// getting only strings from *....*
-                var newregex = new RegExp ("^.*" + (res[1]) + "$");
-                if(newregex.test(dataList[i][realKey]))
-                    sortedList.push(dataList[i]);
-            }
-            else if (secondcase.test(value)){
-                var res = secondcase.exec(value);// getting only strings from *....*
-                var newregex = new RegExp ("^" +(res[3]) + ".*$");
-                if(newregex.test(dataList[i][realKey]))
-                    sortedList.push(dataList[i]);
-            }
+    try {
+        var sortedList: any[] = [];
+        var realKey = findKey(key);
+        var firstcase = new RegExp("^\\*([a-z]+|(\\;|\\-|\\,))([a-z]|(\\;|\\-|\\s|\\,))*$");
+        var secondcase = new RegExp("^([a-z]|(\\;|\\-|\\s|\\,))*([a-z]+|(\\;|\\-|\\,))\\*$");
+        var thirdcase = new RegExp("^\\*([a-z]|(\\;|\\-|\\,))+([a-z]|(\\;|\\-|\\,|\\s))*\\*$");
+        //var regex = /[a-z]+(\\;|\\-|\\s)?[a-z]*(\\,[a-z]+(\\;|\\-|\\s)?[a-z]*)*/
+        for (let i = 0; i < dataList.length; i++) {
+            if (typeof (dataList[i][realKey]) === 'string') {
+                if (firstcase.test(value)) {
+                    var res = firstcase.exec(value);// getting only strings from *....*
+                    var newregex = new RegExp("^.*" + (res[1]) + "$");
+                    if (newregex.test(dataList[i][realKey]))
+                        sortedList.push(dataList[i]);
+                }
+                else if (secondcase.test(value)) {
+                    var res = secondcase.exec(value);// getting only strings from *....*
+                    var newregex = new RegExp("^" + (res[3]) + ".*$");
+                    if (newregex.test(dataList[i][realKey]))
+                        sortedList.push(dataList[i]);
+                }
 
-            else if (thirdcase.test(value)) {
-                var res = thirdcase.exec(value);// getting only strings from *....*
-                var newregex = new RegExp ("^.*" + (res[1]) + ".*$")
-                if(newregex.test(dataList[i][realKey]))
-                    sortedList.push(dataList[i]);
-            }
+                else if (thirdcase.test(value)) {
+                    var res = thirdcase.exec(value);// getting only strings from *....*
+                    var newregex = new RegExp("^.*" + (res[1]) + ".*$")
+                    if (newregex.test(dataList[i][realKey]))
+                        sortedList.push(dataList[i]);
+                }
 
-            else if (dataList[i][realKey] == value) {
-                sortedList.push(dataList[i]);
+                else if (dataList[i][realKey] == value) {
+                    sortedList.push(dataList[i]);
+                }
+            }
+            else if (typeof (dataList[i][realKey]) !== 'string') {
+                Doeverything.fail = true;
             }
         }
-        else if (typeof (dataList[i][realKey]) !== 'string' ) {
-            Doeverything.fail = true;
-        }
+        return sortedList;
     }
-    return sortedList;
+    catch(e) {
+        console.log(e);
+    }
 }
 
 // dataFromLocal(): any {
@@ -210,67 +249,84 @@ function createISList(key: string, value: string, dataList: any[]): any[] {
 
 
 function findKey(key: string) : string {
-    if(key == "courses_dept")
-        return "Subject";
-    if(key == "courses_id")
-        return "Course";
-    if(key == "courses_avg")
-        return "Avg";
-    if(key == "courses_instructor")
-        return "Professor";
-    if(key == "courses_title")
-        return "Title";
-    if(key == "courses_pass")
-        return "Pass";
-    if(key == "courses_fail")
-        return "Fail";
-    if(key == "courses_audit")
-        return "Audit";
-    if(key == "courses_uuid")
-        return "id";
-    else
-        Doeverything.fail = true;
+    try {
+        if (key == "courses_dept")
+            return "Subject";
+        if (key == "courses_id")
+            return "Course";
+        if (key == "courses_avg")
+            return "Avg";
+        if (key == "courses_instructor")
+            return "Professor";
+        if (key == "courses_title")
+            return "Title";
+        if (key == "courses_pass")
+            return "Pass";
+        if (key == "courses_fail")
+            return "Fail";
+        if (key == "courses_audit")
+            return "Audit";
+        if (key == "courses_uuid")
+            return "id";
+        else
+            Doeverything.fail = true;
+    }
+    catch(e) {
+        console.log(e);
+    }
 }
 
 function createModifiedList(list: any, options: any): any {
-    let output: any = {'render': '','result': []};
-    let newlist: any[] = [];
-    let form = Object.keys(options)[2];
-    output['render'] = options[form];
-    let columnsKey = Object.keys(options)[0];
-    let columnsValue = options[columnsKey];
-    if (columnsValue.length == 0){
-        Doeverything.fail = true;
-    }
-    for(let i = 0; i < list.length; i++){
-        let element: any = {};
-        for(let j = 0; j < columnsValue.length; j++){
-            let key = findKey(columnsValue[j]);
-            element[columnsValue[j]] = list[i][key];
+    try {
+        let output: any = {'render': '', 'result': []};
+        let newlist: any[] = [];
+        let form = Object.keys(options)[2];
+        output['render'] = options[form];
+        let columnsKey = Object.keys(options)[0];
+        let columnsValue = options[columnsKey];
+        if (columnsValue.length == 0) {
+            Doeverything.fail = true;
         }
-        newlist.push(element);
+        for (let i = 0; i < list.length; i++) {
+            let element: any = {};
+            for (let j = 0; j < columnsValue.length; j++) {
+                let key = findKey(columnsValue[j]);
+                element[columnsValue[j]] = list[i][key];
+            }
+            newlist.push(element);
+        }
+        let order = Object.keys(options)[1];
+        let orderValue = options[order];
+        newlist.sort(sort_by(orderValue, false, parseFloat));
+        for (let i = 0; i < newlist.length; i++)
+            output['result'].push(newlist[i]);
+        return output;
     }
-    let order = Object.keys(options)[1];
-    let orderValue = options[order];
-    newlist.sort(sort_by(orderValue, false, parseFloat));
-    for(let i = 0; i < newlist.length; i++)
-        output['result'].push(newlist[i]);
-    return output;
+    catch(e) {
+        console.log(e);
+    }
 }
 
 var sort_by = function(field: any, reverse: any, primer: any){
+    try {
 
-    var key = primer ?
-        function(x: any) {return primer(x[field])} :
-        function(x: any) {return x[field]};
+        var key = primer ?
+            function (x: any) {
+                return primer(x[field])
+            } :
+            function (x: any) {
+                return x[field]
+            };
 
-    reverse = !reverse ? 1 : -1;
+        reverse = !reverse ? 1 : -1;
 
-    return function (a: any, b: any) {
-        return a = key(a), b = key(b), reverse * (<any>(a > b) - <any>(b > a));
+        return function (a: any, b: any) {
+            return a = key(a), b = key(b), reverse * (<any>(a > b) - <any>(b > a));
+        }
     }
-}
-
+    catch(e) {
+        console.log(e);
+    }
 }
 
 export default class InsightFacade implements IInsightFacade {
